74a77bb52f99f92cc1eb23a2762572ce7fc168d0
Julie Gankina <qunya11@gmail.com>
2020-01-24 00:23:45 +0300

    getColor function

--- js/stat.js
+++ js/stat.js
@@ -8,13 +8,25 @@ var COLUMN_WIDTH = 40;
 var GAP = 50;
 var MAX_HEIGHT = 150;
 var BAR_Y = CLOUD_Y + 70 + MAX_HEIGHT;
+var specialColor = 'rgba(255, 0, 0, 1)';
+
 
 var renderCloud = function (ctx, x, y, color, width, height) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 };
 
-var renderStatistics = function (ctx, names, times) {
+var getColor = function (param, paramValue, color1, color2) {
+  if (param === paramValue) {
+    return color1;
+  } else {
+    return color2;
+  }
+}
+
+window.renderStatistics = function (ctx, names, times) {
+  var maxTime = Math.round(Math.max.apply(Math, times));
+  var customColor = 'hsl(240, ' + Math.round(Math.random() * 100) + '%, 30%)';
 
   renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)', CLOUD_WIDTH, CLOUD_HEIGHT);
 
@@ -32,7 +44,6 @@ var renderStatistics = function (ctx, names, times) {
     ctx.font = '16px, PT mono';
     ctx.fillText(names[i], columnX, 260);
 
-    var maxTime = Math.round(Math.max.apply(Math, times));
     if (playerTime === maxTime) {
       var barHeight = 150;
     } else {
@@ -40,11 +51,9 @@ var renderStatistics = function (ctx, names, times) {
     }
     ctx.fillText(playerTime, columnX, BAR_Y - barHeight);
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(240, ' + Math.round(Math.random() * 100) + '%, 30%)';
-    }
+
+    ctx.fillStyle = getColor(names[i], 'Вы', specialColor, customColor);
+
     ctx.fillRect(columnX, BAR_Y - barHeight + 10, COLUMN_WIDTH, barHeight);
 
     columnX += COLUMN_WIDTH + GAP;
